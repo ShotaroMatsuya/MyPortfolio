98292640085041d044b194af880c4c7e96a3d49d
ShotaroMatsuya <matsuya@h01.itscom.net>
2020-10-16 19:40:18 +0900

    adding typejs & loader gif

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
+$(window).on("load",function(){
+    $(".loader .inner").fadeOut(500,function(){
+        $(".loader").fadeOut(750);
+    });
+    
+});
+
 $(document).ready(function(){
     //superslides
     $('#slides').superslides({
         animation:'slide',
         play:5000,
         pagination:false
+    });
+    var typed = new Typed(".typed",{
+        strings:["Web Developer.","Student.","Creative Design.","Doctor."],
+        typeSpeed:70,
+        loop:true,
+        startDelay:1000,
+        showCursor:false
     });
 
 
